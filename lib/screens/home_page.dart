3086a87938bba7c56fd36dd9151bab7b717027e7
ramazanov7 <ramazan.alibektyn@gmail.com>
2024-02-06 11:46:08 +0600

    created the 10 container

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,9 +13,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(),
-      body: Column(),
+      backgroundColor: const Color.fromARGB(255, 29, 29, 29),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 52, 52, 52),
+        // elevation: 5.0,
+        title: Text('News',
+          style: TextStyle(
+            color: Colors.white
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 20, left: 15, right: 15),
+        child: ListView.builder(
+          // lenght of news feeds. ex: 10
+          itemCount: 10,
+          itemBuilder:(context, index) {
+
+            // Section
+            return ListTile(
+              contentPadding: EdgeInsets.only(top: 10),
+              title: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 0, 186, 192),
+                  borderRadius: BorderRadius.circular(20)
+                ),
+              ),
+            );
+          },
+        ),
+      )
     );
   }
 }
