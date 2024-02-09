9968f4ac9f478dfaa96254b8fcf3e94dd300cbca
ramazanov7 <ramazan.alibektyn@gmail.com>
2024-02-09 10:14:22 +0600

    готовый дизайн

--- lib/screens/details_screen.dart
+++ lib/screens/details_screen.dart
@@ -24,32 +24,125 @@ class DetailsScreen extends StatelessWidget {
               width: 70,
               margin: EdgeInsets.only(top: 16, left: 16),
               decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(8)
+                color: const Color.fromARGB(255, 34, 34, 34),
+                borderRadius: BorderRadius.circular(8),
+                
               ),
               child: IconButton(
                 onPressed: () {
                   Navigator.pop(context);
                 },
                 icon: Icon(Icons.arrow_back),
+                color: Colors.white,
               ),
             ),
 
             // 
-            backgroundColor: Colors.teal,
             expandedHeight: 500,
             pinned: true,
             floating: true,
             flexibleSpace: FlexibleSpaceBar(
-              title: Text(movie.title,
+              title: Text(
+                movie.title,
                 style: TextStyle(
                   fontSize: 20,
                 ),
               ),
-              background: Image.network(
-                '${NewsService.imagePath}${movie.backdropPath}',
-                filterQuality: FilterQuality.high,
-                fit: BoxFit.cover,
+              background: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(24),
+                  bottomRight: Radius.circular(24)
+                ),
+                child: Image.network(
+                  '${NewsService.imagePath}${movie.backdropPath}',
+                  filterQuality: FilterQuality.high,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  Text(
+                    movie.overview,
+                    style: TextStyle(
+                      fontSize: 15,
+                    ),
+                  ),
+                  SizedBox(height: 15,),
+                  Container(
+                    child: Row(
+                      children: [
+
+                        // Release date 
+                        Container(
+                          
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Release date: ',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              Text(
+                                movie.releaseDate,
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        SizedBox(width: 10,),
+
+                        // Vote average 
+                        Container(
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                'Rating: ',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              ),
+                              // Icon(
+                              //   Icons.star,
+                              //   color: Colors.yellow,
+                              //   size: 15,
+                              // ),
+                              Text(
+                                '${movie.voteAverage.toString()}',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
